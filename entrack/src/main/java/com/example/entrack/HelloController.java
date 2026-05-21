c8a2095f106b08dd91a62a21eec3f497ca0ca58d
udita das <uditadas619@gmail.com>
2026-05-21 16:25:38 +0530

    Employee data added

--- entrack/src/main/java/com/example/entrack/HelloController.java
+++ entrack/src/main/java/com/example/entrack/HelloController.java
@@ -4,11 +4,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String home() {
         return "Welcome";
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "John", "IT"));
+
+        employees.add(new Employee(2, "Udita", "IT"));
+        employees.add(new Employee(3, "Rahul", "IT"));
+        return employees;
     }
 }
