3eee55c2de67587e4e2adf60cc1fcccf413afd17
udita das <uditadas619@gmail.com>
2026-05-26 14:28:23 +0530

    Added Swagger OpenAPI documentation

--- entrack/src/main/java/com/example/entrack/EmployeeController.java
+++ entrack/src/main/java/com/example/entrack/EmployeeController.java
@@ -5,9 +5,8 @@ import com.example.entrack.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
-
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -23,22 +22,26 @@ public class EmployeeController {
         return "Welcome";
     }
 
+    @Operation(summary = "Get all employees")
     @GetMapping
     public List<Employee> getEmployees() {
         return employeeService.getEmployees();
     }
 
+    @Operation(summary = "Get employee by id")
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
         return employeeService.getEmployeeById(id);
     }
 
+    @Operation(summary = "Delete employee by id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployeeById(@PathVariable int id) {
         employeeService.deleteEmployee(id);
     }
 
+    @Operation(summary = "Update employee")
     @PutMapping("/{id}")
     public Employee updatedEmployee(
             @PathVariable int id,
@@ -52,6 +55,7 @@ public class EmployeeController {
         return employeeService.updateEmployee(id, employee);
     }
 
+    @Operation(summary = "Add new employee")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(
